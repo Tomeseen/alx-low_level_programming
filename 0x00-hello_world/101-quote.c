c4694ab053836de2ea8b8ee53bf7bd2766602d52
Tomeseen <akinbolatomisin23@gmail.com>
2022-09-09 15:36:28 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints the phrase
+ * "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	output(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 2);
+	return (1);
+}
