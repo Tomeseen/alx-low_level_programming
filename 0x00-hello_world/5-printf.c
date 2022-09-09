29457edbec4a35ee81779864cf311e3fa8610fbf
Tomeseen <akinbolatomisin23@gmail.com>
2022-09-09 13:30:51 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 
 /**
  * main - prints the phrase
- * with proper grammar, but the outcome is a piece of art,
+ * "with proper grammar, but the outcome is a piece of art,"
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
-	printf("\with proper grammar, but the outcome is a piece of art,);
+	printf("\"with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
