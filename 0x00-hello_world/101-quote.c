c5b11f41ccc68b0b756518806e99a35f01ce3fcf
Tomeseen <akinbolatomisin23@gmail.com>
2022-09-09 15:49:58 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <unistd.h>
 /**
  * main - prints the phrase without printf or puts
  * Return: 1 if succes
  */
 int main(void)
 {
-	output(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
