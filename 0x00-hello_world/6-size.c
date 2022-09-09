ce23dd8965c2022fdc24de20ca91b515a5e91dbb
Tomeseen <akinbolatomisin23@gmail.com>
2022-09-09 14:17:18 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints the size of various types on the computer it is compiled and run on
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	int intType
+	float floatType
+	char charType
+	long int longintType
+	long long int longlongintType
+
+	printf("Size of int: 4 bytes\n", sizeof(intType));
+	printf("Size of float: 4 bytes\n", sizeof(floatType));
+	printf("Size of char: 1 byte\n", sizeof(charType));
+	printf("Size of long int: 8 bytes\n", sizeof(longintType));
+	printf("Size of long long int: 8 bytes\n", sizeof(longlongintType));
+
+	return (0);
+}
+
