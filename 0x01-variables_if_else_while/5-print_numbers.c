c58857e386467966e12244700b9bb3d7e0c46a29
Tomeseen <akinbolatomisin23@gmail.com>
2022-09-11 14:53:59 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - code block
+ * Description - prints all single digits
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for  (num = 0; num < 10; num++)
+		printf("%i", num);
+	printf("\n");
+	return (0);
+}
