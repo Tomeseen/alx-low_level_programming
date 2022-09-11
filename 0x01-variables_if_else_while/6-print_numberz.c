7831c44eb53fb51a2a6242fd26dc504383729bc0
Tomeseen <akinbolatomisin23@gmail.com>
2022-09-11 15:00:14 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - code block
+ * Description - prints all single digit numbers of base 10
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	putchar('\n');
+	return (0);
+}
