183bcb2c1d825e7eaa828e5b88c3aade3cd18162
Tomeseen <akinbolatomisin23@gmail.com>
2022-09-15 16:13:42 -0700

    debugging project

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - tests functions that prints if int is '+' or '-'
+ * main - tests functions that prints if integer is positive or negative
  * Return: 0
  */
 int main(void)
