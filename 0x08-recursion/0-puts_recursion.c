a474b967b10c537570d0ef6dcb8b8221a87f61bb
Tomeseen <akinbolatomisin23@gmail.com>
2022-09-28 07:21:35 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,10 +10,10 @@
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-		_putchar('\n');
+		_putchar('\n')
 	else
 	{
-		_putchar(s);
-		_putchar(s + 1);
+		_putchar(s[0]);
+		puts_recursion(s + 1);
 	}
 }
