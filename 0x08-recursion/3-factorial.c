624747532bebfe09f836fe5ae4d10f0038b8df5c
Tomeseen <akinbolatomisin23@gmail.com>
2022-09-28 07:57:30 -0700

    recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * factorial - entry point
+ * Description - returns the factorial of a given number
+ * @n: given number
+ * Return: the factorial of n,
+ * if n is lower than 0 return -1
+ */
+
+int factorial(int n)
+{
+	int i;
+
+	if (n == 0)
+		return (1);
+	else if
+		if (n < 0)
+			return (-1);
+
+	i = factorial(n - 1);
+	return (n * i);
+}
