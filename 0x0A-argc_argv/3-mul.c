2752720458b787a3ee2dc3a2f5260f07af647a3c
Tomeseen <akinbolatomisin23@gmail.com>
2022-10-05 04:16:33 -0700

    argc, argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers followed by a new line
+ * @argc:number of arguments
+ * @argv: array of pointers to argument
+ * Return: return 0 if there are two arguments,
+ * 1 if there is 1 argument
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, prod;
+
+	if (argc != 3)
+	{
+		printf("ERROR\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	prod = num1 * num2;
+
+	printf("%d\n", prod);
+
+	return (0);
+}
