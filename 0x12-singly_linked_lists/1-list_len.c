d7f663043f805b2f6e7ddbd9eb5ff81a6412cf31
Tomeseen <akinbolatomisin23@gmail.com>
2022-10-20 01:41:37 -0700

    singly linked list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,19 +4,17 @@
  * list_len - returns the number of elements in
  *            a linked list_t list.
  * @h: The linked list_t list.
- * @elements: a counter var for size_t (nodes)
- *
  * Return: The number of elements in h.
  */
 size_t list_len(const list_t *h)
 {
-	size_t elements = 0;
+	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		elements++;
 		h = h->next;
+		count++;
 	}
 
-	return (elements);
+	return (count);
 }
